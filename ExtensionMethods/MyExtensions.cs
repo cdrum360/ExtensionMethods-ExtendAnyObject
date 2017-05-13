f227b3ce3751b07b14ed813ca9547006b48c9b2a
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 12:49:07 -0400

    Add project files.

---
+++ ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static void DisplayAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: => {1}\n", obj.GetType().Name, Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+
+        public static int ReverseDigits(this int i)
+        {
+            char[] digits = i.ToString().ToCharArray();
+            Array.Reverse(digits);
+            string newDigits = new string(digits);
+
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Media;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // int
+            int myInt = 12345678;
+            myInt.DisplayAssembly();
+
+            #region More Examples
+            
+            // DataSet
+            DataSet myDataSet = new DataSet();
+            myDataSet.DisplayAssembly();
+
+            // SoundPlayer
+            SoundPlayer mySoundPlayer = new SoundPlayer();
+            mySoundPlayer.DisplayAssembly();
+
+            // DateTime
+            DateTime date = new DateTime();
+            date.DisplayAssembly();
+
+            // String
+            string myString = "CD";
+            myString.DisplayAssembly();
+
+            #endregion
+
+            // Use new integer functionality.
+            Console.WriteLine("Value of myInt:           {0}", myInt);
+            Console.WriteLine("Reversed digits of myInt: {0}", myInt.ReverseDigits());
+
+            Console.ReadLine();
+        }
+    }
+}
